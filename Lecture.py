f1650ed1579cefdaa04f089e1347ecbba1a16e64
EspaceRaspberryFrancais <36486098+EspaceRaspberryFrancais@users.noreply.github.com>
2018-02-15 10:29:52 +0100

    Add files via upload

---
+++ Lecture.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+
+reader = SimpleMFRC522.SimpleMFRC522()
+
+try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+        GPIO.cleanup()
