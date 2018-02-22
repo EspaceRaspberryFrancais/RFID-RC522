24aa6fd9dec4a03266534aa1e2ca06581609372f
EspaceRaspberryFrancais <36486098+EspaceRaspberryFrancais@users.noreply.github.com>
2018-02-22 13:34:58 +0100

    Update Lecture.py

--- Lecture.py
+++ Lecture.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
+# Version modifiee de la librairie https://github.com/mxgxw/MFRC522-python
 
 import RPi.GPIO as GPIO
-import SimpleMFRC522
+import MFRC522
+import signal
 
-reader = SimpleMFRC522.SimpleMFRC522()
+continue_reading = True
 
-try:
-    id, text = reader.read()
-    print(id)
-    print(text)
-finally:
+# Fonction qui arrete la lecture proprement 
+def end_read(signal,frame):
+    global continue_reading
+    print ("Lecture terminée")
+    continue_reading = False
     GPIO.cleanup()
+
+signal.signal(signal.SIGINT, end_read)
+MIFAREReader = MFRC522.MFRC522()
+
+
+while continue_reading:
+    
+    # Detecter les tags
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # Une carte est detectee
+    if status == MIFAREReader.MI_OK:
+        print ("Carte detectee")
+    
+    # Recuperation UID
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    if status == MIFAREReader.MI_OK:
+        print ("UID de la carte : "+str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3]))
+    
+        # Clee d authentification par defaut
+        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        
+        # Selection du tag
+        MIFAREReader.MFRC522_SelectTag(uid)
+
+        # Authentification
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+        if status == MIFAREReader.MI_OK:
+            MIFAREReader.MFRC522_Read(8)
+            MIFAREReader.MFRC522_StopCrypto1()
+        else:
+            print ("Erreur d\'Authentification")
