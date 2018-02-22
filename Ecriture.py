e7f3a2ddaa71c49f54a332db3d80e378cc6b3df2
EspaceRaspberryFrancais <36486098+EspaceRaspberryFrancais@users.noreply.github.com>
2018-02-22 13:36:12 +0100

    Update Ecriture.py

--- Ecriture.py
+++ Ecriture.py
@@ -1,14 +1,70 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
+# Version modifiee de la librairie https://github.com/mxgxw/MFRC522-python
 
 import RPi.GPIO as GPIO
-import SimpleMFRC522
+import MFRC522
+import signal
 
-reader = SimpleMFRC522.SimpleMFRC522()
+continue_reading = True
 
-try:
-    text = raw_input('Donnée :')
-    print("Placer le tag RFID ...")
-    reader.write(text)
-    print("OK")
-finally:
+# Fonction qui arrete la lecture proprement 
+def end_read(signal,frame):
+    global continue_reading
+    print ("Lecture terminée")
+    continue_reading = False
     GPIO.cleanup()
+
+signal.signal(signal.SIGINT, end_read)
+MIFAREReader = MFRC522.MFRC522()
+
+data = []
+texte = raw_input("Texte ?\n")
+for c in texte:
+    if (len(data)<16):
+        data.append(int(ord(c)))
+while(len(data)!=16):
+    data.append(0)
+
+while continue_reading:
+      
+    # Detecter les tags
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # Une carte est detectee
+    if status == MIFAREReader.MI_OK:
+        print ("Carte detectee")
+    
+    # Recuperation UID
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    if status == MIFAREReader.MI_OK:
+
+        # Print UID
+        print ("UID de la carte : "+str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3]))
+    
+        # Clee d authentification par defaut
+        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        
+        # Selection du tag
+        MIFAREReader.MFRC522_SelectTag(uid)
+
+        # Authentification
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+        if status == MIFAREReader.MI_OK:
+            print ("Le secteur 8 contient actuellement : ")
+            MIFAREReader.MFRC522_Read(8)
+
+            print ("Ecriture ...")
+            MIFAREReader.MFRC522_Write(8, data)
+
+            print ("Le secteur 8 contient maintenant : ")
+            MIFAREReader.MFRC522_Read(8)
+
+            # Stop
+            MIFAREReader.MFRC522_StopCrypto1()
+            continue_reading = False
+
+        else:
+            print ("Erreur d authentication")
